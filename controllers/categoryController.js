0e3f68e6f692f456a70029cc64d98954d399c658
salsabeel10 <salsabeelibrahim10@gmail.com>
2025-09-19 16:28:52 +0400

    get annd delete category done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -19,3 +19,30 @@ exports.createCategory = async(req,res)=>{
         return res.status(500).json({error: error.message})
     }
 }
+
+exports.getCategory = async(req,res)=>{
+    try{
+        const allCategories = await prisma.category.findMany();
+
+        return res.status(201).json({allCategories})
+
+    } catch (error){
+        return res.status(500).json({error: error.message})
+    }
+}
+
+exports.deleteCategory = async(req,res)=>{
+    try {
+        const {id} =req.params;
+        const deleteCategory =await prisma.category.delete({
+            where:{id:parseInt(id)}
+        })
+
+        return res.status(200).json({
+            message: "category deleted Sucessfully",
+            deleteCategory
+        });
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
+}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -3,4 +3,8 @@ const categoryController = require('../controllers/categoryController')
 
 router.post('/',categoryController.createCategory)
 
+router.get('/',categoryController.getCategory)
+
+router.delete('/:id',categoryController.deleteCategory)
+
 module.exports =router
